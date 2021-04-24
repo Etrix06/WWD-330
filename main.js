bcfe3cd5465e8f4216fadd67a7d0777198261e9f
Etrix06 <82925150+Etrix06@users.noreply.github.com>
2021-04-24 11:18:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function myFunction() {
   document.getElementById("jsTest").innerHTML="This is a test to see if my js file is working.";
 }
+
+const links = [
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  }
+  
+  
+]
