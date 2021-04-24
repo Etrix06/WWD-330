22053bcd7d275b2c711fde232001b0a95bd51d72
Etrix06 <82925150+Etrix06@users.noreply.github.com>
2021-04-24 11:56:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function myFunction() {
-  document.getElementById("jsTest").innerHTML="This is a test to see if my js file is working.";
-}
-
-function makeLinkList() {
-  document.getElementById(
 const links = [
   {
     label: "Week1 notes",
     url: "week1/index.html"
   }
-  
-  
-]
+] 
+
+function myFunction() {
+  document.getElementById("jsTest").innerHTML="This is a test to see if my js file is working.";
+}
+
+function makeLinkList() {
+  document.getElementById("indexList").innerHTML= links;
+}
+
