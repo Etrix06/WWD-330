695dbb0b307cafa7370f205997a6260c90b162b6
etrix06 <alc18005@byui.edu>
2021-05-11 21:35:56 -0700

    hero

--- week4/notes4/main.js
+++ week4/notes4/main.js
@@ -1,6 +1,18 @@
 const form = document.forms['search'];
-const [input,button] = form.elements;
+
 const input = form['searchInput']
-const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'), false);
-input.addEventListener('blur', () => alert('blurred'), false);
+input.value = 'Search Here';
+input.addEventListener('focus', function(){
+  if (input.value==='Search Here') {
+      input.value = '' 
+  }
+}, false);
+input.addEventListener('blur', function(){
+  if(input.value === '') {
+      input.value = 'Search Here';
+  } }, false);
+form.addEventListener ('submit', search, false);
+function search(event) {
+  alert(`You Searched for: ${input.value}`);
+  event.preventDefault();
+}
