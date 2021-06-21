1d538d20a4375df7c65cce9de0a2071eccae6d12
etrix06 <alc18005@byui.edu>
2021-06-21 12:51:19 -0700

    drum kit and week 9

---
+++ week9/week9.js
@@ -0,0 +1,20 @@
+const links = [
+  { label: "Notes and Questions",                url: "../week9/notes9.html" },
+  { label: "Drum Kit",                url: "../week8/practice9.html"}
+
+]; 
+
+
+
+function addToIndex() {
+    for (let i = 0; i < links.length; i++ ) {
+      var a = document.createElement("a");
+      var linkText = document.createTextNode(links[i].label);
+      a.appendChild(linkText);
+      a.title = "This is the " + links[i].label + " link";
+      a.href = links[i].url;
+      var y = document.createElement("LI");
+      y.appendChild(a);
+      document.getElementById("indexList").appendChild(y);
+    }
+}
