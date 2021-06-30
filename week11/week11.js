a70e4d3655a816ad6e6b3fc8100b00abe178fa28
etrix06 <alc18005@byui.edu>
2021-06-29 21:43:44 -0700

    ch

--- week11/week11.js
+++ week11/week11.js
@@ -1,7 +1,7 @@
 const links = [
   { label: "Notes and Questions",                url: "../week11/notes11.html"          },
   { label: "Week 11 Team activity",              url: "../week11/index.html"            },
-  { label: "Weather App",                        url: "../../weatherApp/weatherApp.html"},
+  { label: "Weather App",                        url: "../WWD-330/weatherApp/weatherApp.html"},
   { label: "Miles App",                          url: "../week11/miles.html"            }
 
 ]; 
