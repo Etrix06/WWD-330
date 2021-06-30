6660cd7b030ad9596e3b42eb838e006c7e387fd3
etrix06 <alc18005@byui.edu>
2021-06-29 21:50:42 -0700

    c

--- week11/week11.js
+++ week11/week11.js
@@ -1,8 +1,8 @@
 const links = [
-  { label: "Notes and Questions",                url: "../week11/notes11.html"          },
-  { label: "Week 11 Team activity",              url: "../week11/index.html"            },
-  { label: "Weather App",                        url: "../WWD-330/weatherApp/weatherApp.html"},
-  { label: "Miles App",                          url: "../week11/miles.html"            }
+  { label: "Notes and Questions",        url: "../week11/notes11.html"          },
+  { label: "Week 11 Team activity",      url: "../week11/index.html"            },
+  { label: "Weather App",                url: "../WWD-330/weatherApp/weatherApp.html"},
+  { label: "Miles App",                  url: "../week11/miles.html"            }
 
 ]; 
 
