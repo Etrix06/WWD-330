03ae0dcd45845174445378f88af9b2fc4b9332b7
etrix06 <alc18005@byui.edu>
2021-06-29 21:32:27 -0700

    Added Miles App

--- week11/week11.js
+++ week11/week11.js
@@ -1,7 +1,8 @@
 const links = [
   { label: "Notes and Questions",                url: "../week11/notes11.html"          },
   { label: "Week 11 Team activity",              url: "../week11/index.html"            },
-  { label: "Weather App",                        url: "../../weatherApp/weatherApp.html"}
+  { label: "Weather App",                        url: "../../weatherApp/weatherApp.html"},
+  { label: "Miles App",                          url: "../week11/miles.html"            }
 
 ]; 
 
