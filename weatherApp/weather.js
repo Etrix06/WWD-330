c858cb52b5a7f805f99deebb12992c0937b58247
etrix06 <alc18005@byui.edu>
2021-07-13 22:07:27 -0700

    ch

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -1,5 +1,5 @@
 class Fetch {
-  async getCurrent(city) {
+  async getCurrentCity(city) {
     const myKey = "b289d6d45af893f5469e8c958107b6d7";
 
     //make request to url
@@ -11,20 +11,23 @@ class Fetch {
     const data = await response.json();
 
     console.log(data);
+   
 
     return data;
   }
 }
-
-class UI {
+//UI is now Display
+class Display {
   constructor() {
-    this.uiContainer = document.getElementById("display");
-    this.city;
-    this.defaultCity = "Rexburg";
+    //uiContainer is now display
+    this.display = document.getElementById("display");
+    //this.city;
+    //this.defaultCity = "Rexburg";
   }
 
-  populateUI(data) {
-    this.uiContainer.innerHTML = `
+  displayData(data) {
+    //console.log("Default city is - " + this.defaultCity);
+    this.display.innerHTML = `
         
         <div>
             <div>
@@ -39,7 +42,7 @@ class UI {
         
         `;
   }
-
+/*
   clearUI() {
     uiContainer.innerHTML = "";
  
@@ -61,33 +64,91 @@ class UI {
 
   clearLS() {
     localStorage.clear();
+  } */
+}
+
+class Display5days {
+  constructor() {
+    //uiContainer is now display
+    this.display = document.getElementById("display");
+    //this.city;
+    //this.defaultCity = "Rexburg";
+  }
+
+  display5dayForcast(data) {
+    //console.log("Default city is - " + this.defaultCity);
+    console.log(data.list[6].dt_txt);
+    const date1 = new Date(data.list[0].dt_txt).toLocaleDateString('en-US');
+    const date2 = new Date(data.list[6].dt_txt).toLocaleDateString('en-US');
+    const date3 = new Date(data.list[14].dt_txt).toLocaleDateString('en-US');
+    const formattedDate1 = date1.split('/').join('-');
+    const formattedDate2 = date2.split('/').join('-');
+    const formattedDate3 = date3.split('/').join('-');
+    console.log(formattedDate1);
+    console.log(formattedDate2);
+    console.log(formattedDate3);
+
+    this.display.innerHTML = `
+        
+        <div>
+            <div>
+                <h3>${data.city.name}, ${data.city.country}</h3>
+                <h4>${formattedDate1}</h4><h4>${formattedDate2}</h4><h4>${formattedDate3}</h4>
+                
+                
+            </div>
+        </div>
+        
+        `;
   }
 }
 
-const ft = new Fetch();
-const ui = new UI();
+
+const fetchClassObj = new Fetch();
+//ui is now displayClass
+const displayClassObj = new Display();
+const displayForecastObj = new Display5days();
 
 const search = document.getElementById("searchCity");
 const button1 = document.getElementById("submit");
+const button2 = document.getElementById("forecast");
 
 button1.addEventListener("click", () => {
   const currentVal = search.value;
 
-  ft.getCurrent(currentVal).then((data) => {
-    ui.populateUI(data);
-    ui.saveToLocalStorage(data);
+  fetchClassObj.getCurrentCity(currentVal).then((data) => {
+    displayClassObj.displayData(data);
+    //ui.saveToLocalStorage(data);
   });
 });
 
-//event listener for local storage and to clear
+search.addEventListener("keyup", function(event) {
+  if (event.key === 'Enter') {
+    const currentVal = search.value;
 
+    fetchClassObj.getCurrentCity(currentVal).then((data) => {
+      displayClassObj.displayData(data);
+  });
+};
+});
+
+button2.addEventListener("click", () => {
+  const currentVal = search.value;
+
+  fetchClassObj.getCurrentCity(currentVal).then((data) => {
+    displayForecastObj.display5dayForcast(data);
+    //ui.saveToLocalStorage(data);
+  });
+});
+//event listener for local storage and to clear
+/*
 window.addEventListener("DOMContentLoaded", () => {
   //clearUI();
   //clearLS();
   
-  const dataSaved = ui.getFromLocalStorage();
-  ui.populateUI(dataSaved);
+  //const dataSaved = ui.getFromLocalStorage();
+  //ui.displayData(dataSaved);
 });
-
+*/
 
 
